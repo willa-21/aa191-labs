887b3446a4c785f6b7667ac6cb3f0ef948510cd3
willakurland <willamei@g.ucla.edu>
2022-04-13 13:52:41 -0700

    Week-2lab

--- Week-2/js/init.js
+++ Week-2/js/init.js
@@ -12,7 +12,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // create a function to add markers
 function addMarker(lat,lng,title,message){
     console.log(message)
-    L.marker([lat,lng]).addTo(map).bindPopup(`<h2>${title}</h2> <h3>${message}</h3>`)
+    L.circleMarker([lat,lng]).addTo(map).bindPopup(`<h2>${title}</h2> <h3>${message}</h3>`)
     return message
 }
 
