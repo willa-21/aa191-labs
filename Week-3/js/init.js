c3e5653087e0905348755701f82770d2e3377feb
willakurland <willamei@g.ucla.edu>
2022-04-18 17:26:33 -0700

    Week3 commit

---
+++ Week-3/js/init.js
@@ -0,0 +1,25 @@
+ // declare variables
+let mapOptions = {'center': [34.0709,-118.444],'zoom':5}
+
+// use the variables
+const map = L.map('the_map').setView(mapOptions.center, mapOptions.zoom);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+// create a function to add markers
+function addMarker(lat,lng,title,message){
+    console.log(message)
+    L.marker([lat,lng]).addTo(map).bindPopup(`<h2>${title}</h2> <h3>${message}</h3>`)
+    return message
+}
+
+fetch("map.geojson")
+    .then(response => {
+        return response.json();
+    })
+    .then(data =>{
+        // Basic Leaflet method to add GeoJSON data
+        L.geoJSON(data).addTo(map)
+    });
